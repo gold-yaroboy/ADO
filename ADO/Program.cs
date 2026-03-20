285a0d1fbc252adedeec4250098eaf5861d07aaa
ilyav <ilyav@ILIA>
2026-03-20 22:35:10 +0500

    Connector begin

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -12,34 +12,22 @@ namespace ADO
 		static void Main(string[] args)
 		{
 			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-			Console.WriteLine(connection_string);
-
-			SqlConnection connection = new SqlConnection(connection_string);
-			connection.Open();
+			
+			Connector connector = new Connector(connection_string);
 
 			string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-			SqlCommand command = new SqlCommand(cmd, connection);
 
-			SqlDataReader reader = command.ExecuteReader();
-			for (int i = 0; i < reader.FieldCount; i++)
-				Console.Write(reader.GetName(i)+"\t");
+			connector.Select(cmd);
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT (*) FROM Movies")}");
 
-			Console.WriteLine();
+			connector.Select("SELECT * FROM Directors");
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT (*) FROM Directors")}");
 
-			while (reader.Read())
-			{
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-				for (int i = 0; i < reader.FieldCount; i++)
-					Console.Write($"{reader[i]}\t\t");
+			//command.CommandText = "SELECT COUNT (*) FROM Movies";
+			//Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
 
-				Console.WriteLine();
-			}
-			reader.Close();
-
-			command.CommandText = "SELECT COUNT (*) FROM Movies";
-			Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
-
-			connection.Close();
+			//connection.Close();
 		}
+		
 	}
 }
