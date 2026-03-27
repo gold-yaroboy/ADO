e653d7adef4ef37f957f20a9ffb8be1edfd81767
ilyav <ilyav@ILIA>
2026-03-27 19:50:54 +0500

    Connector DLL DONE

---
+++ DLLtest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data.SqlClient.SqlException;
+using DBtools;
+
+namespace DLLtest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Connector connector = new Connector(
+				"Data Source=(localdb)\\MSSQLLocalDB;" +
+				"Initial Catalog=Movies_PV_521;Integrated Security=True;" +
+				"Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;" +
+				"ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			connector.Select("SELECT * FROM Directors");
+		} 
+	}
+}
